2a705cc1419fa90ea6db71a6f5f403a84ba399ca
KyLinx <kylinxyl@pku.edu.cn>
2016-03-20 23:32:19 +0800

    作业3和课堂3

---
+++ 徐玉麟/作业3/2-super-map.rkt
@@ -0,0 +1,23 @@
+#lang racket
+(define (map op lst)
+  (if (null? lst)
+      '()
+      (cons (op (car lst))
+            (map op (cdr lst)))))
+  
+(define (super-map op . w)
+  (if (null? (car w))
+      '()
+      (cons
+       (apply op (map car w))
+       (apply super-map (cons op (map cdr w))))))
+(define (myloop)
+  (let ((a (read))
+        (b (read))
+        (c (read)))
+    (if (eq? a eof)
+        (void)
+        (begin (displayln (super-map + a b c)) 
+               (displayln (super-map (lambda (x y) (+ x (* 2 y) )) a b ))
+               (myloop)))))
+(myloop)
